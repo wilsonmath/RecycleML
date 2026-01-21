f8ada6e32c53a067c4611a7e41f433d01dbee16c
Wilson <wilsonmath55@gmail.com>
2026-01-21 20:23:02 +0800

    V2 CSS+New Model
    Added style.css and a new model with 70% acc (according to training data, hope I didnt overfit it)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const modelURL = "https://wilsonmath.github.io/RecycleML/models/model.json";
 const metadataURL = "https://wilsonmath.github.io/RecycleML/models/metadata.json";
 
-let model, webcam, labelContainer, maxPredictions;
+let model, labelContainer, maxPredictions;
 async function init() {
   model = await tmImage.load(modelURL, metadataURL);
   maxPredictions = model.getTotalClasses();
@@ -16,13 +16,27 @@ async function init() {
 }
 
 async function predict(input) {
-  const prediction = await model.predict(input);
+  var prediction = await model.predict(input);
+  var bestPrediction = null;
+  var maxProbability = 0;
   for (let i = 0; i < maxPredictions; i++) {
-    const classPrediction = `${prediction[i].className}: ${prediction[i].probability.toFixed(2)}`;
-    labelContainer.childNodes[i].innerHTML = classPrediction;
+    if (prediction[i].probability > maxProbability) {
+      maxProbability = prediction[i].probability;
+      bestPrediction = prediction[i];
+    }
   }
-}
 
+  if (labelContainer.innerHTML == "Result") {
+    labelContainer.innerHTML = "Verifying Result"
+  }
+  if (bestPrediction) {
+    var resultText = bestPrediction.className;
+    if (maxProbability > 0.5) {
+      labelContainer.innerHTML = resultText
+    } else { 
+      labelContainer.innerHTML = "Uncertain, Please try Again"
+    }
+  }}
 async function handleImageUpload(event) {
   const file = event.target.files[0];
   if (!file) return;
@@ -31,9 +45,10 @@ async function handleImageUpload(event) {
   img.src = URL.createObjectURL(file);
   const container = document.getElementById("picture-container");
   container.innerHTML = "";
-  img.width = 200;
-  img.height = 200;
+  img.width = 300;
+  img.height = 300;
   container.appendChild(img);
+  labelContainer.innerHTML = "Verifying Result"
   await predict(img);
   };
 
